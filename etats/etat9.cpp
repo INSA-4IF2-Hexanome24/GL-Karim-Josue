562f1abe4cb946719c64132f2e066e672d9d1f7e
Kakim <163103520+kakiim@users.noreply.github.com>
2026-02-25 11:02:26 +0100

    Implement symbol handling in Etat9 Transition method
    Added handling for multiple symbol cases in Transition method.

--- etats/etat9.cpp
+++ etats/etat9.cpp
@@ -1,7 +1,29 @@
 #include "etat9.h"
+#include "symboleNT.h"
+#include "symbole.h"
+#include "automate.h"
 #include <iostream>
 
 void Etat9::Transition(Symbole *s, Automate *a) {
+if (s == nullptr || a == nullptr) return;
+
+   switch ((int)*s) {
+      case PLUS:
+         a->Reduire(4, new SymboleE());
+         break;
+      case MULT:
+         a->Reduire(4, new SymboleE());
+         break;
+      case CLOSEPAR:
+         a->Reduire(4, new SymboleE());
+         break;
+      case FIN:
+         a->Reduire(4, new SymboleE());
+         break;
+      default:
+         break;
+   }
+   
 }
 
 void Etat9::Affiche() {
