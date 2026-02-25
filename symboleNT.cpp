d8d0db54a86b3d8d10003b33657e4f52f4740caf
Josue Vega <josuevegavalbuena@gmail.com>
2026-02-25 09:46:26 +0100

    Changements pour symboleNT

--- symboleNT.cpp
+++ symboleNT.cpp
@@ -3,8 +3,59 @@
 
 void SymboleNT::Affiche() {
    if (ident >= 0 && ident < 2) {
-      cout << EtiquettesNT[ident];
+      cout << EtiquettesNT[ident] << "[" << valeur << "]";
    } else {
       cout << "SymboleNT(?)";
    }
+}
+
+int SymboleNT::eval() const {
+   return valeur;
+}
+
+void SymboleE::Affiche() {
+   cout << "E[" << valeur << "]";
+}
+
+int SymboleE::eval() const {
+   if (leftOperand == nullptr || rightOperand == nullptr) {
+      return valeur;
+   }
+
+   int left = leftOperand->eval();
+   int right = rightOperand->eval();
+
+   switch (operateur) {
+      case PLUS:
+         return left + right;
+      case MULT:
+         return left * right;
+      default:
+         return left;
+   }
+}
+
+void SymboleE::defineOp(SymboleNT *left, int op, SymboleNT *right) {
+   leftOperand = left;
+   operateur = op;
+   rightOperand = right;
+}
+
+void SymboleEPRIM::Affiche() {
+   cout << "EPRIM[";
+   if (e != nullptr) {
+      e->Affiche();
+   }
+   cout << "]";
+}
+
+int SymboleEPRIM::eval() const {
+   if (e != nullptr) {
+      return e->eval();
+   }
+   return valeur;
+}
+
+void SymboleEPRIM::setE(SymboleE *expression) {
+   e = expression;
 }
--- symboleNT.h
+++ symboleNT.h
@@ -11,7 +11,48 @@ const string EtiquettesNT[] = { "E", "EPRIM" };
 
 class SymboleNT : public Symbole {
    public:
-      SymboleNT(int i) : Symbole(i) {}
+      SymboleNT(int i, int v = 0) : Symbole(i), valeur(v) {}
       virtual ~SymboleNT() { }
+      
       virtual void Affiche() override;
+      
+      virtual int eval() const;
+      
+    
+      void setValeur(int v) { valeur = v; }
+   
+      int getValeur() const { return valeur; }
+
+   protected:
+      int valeur; 
+};
+
+class SymboleE : public SymboleNT {
+   public:
+      SymboleE(int v = 0) : SymboleNT(E, v), leftOperand(nullptr), rightOperand(nullptr), operateur(ERREUR) {}
+      virtual ~SymboleE() { }
+
+      virtual void Affiche() override;
+      virtual int eval() const override;
+
+      void defineOp(SymboleNT *left, int op, SymboleNT *right);
+
+   private:
+      SymboleNT *leftOperand;
+      SymboleNT *rightOperand;
+      int operateur;
+};
+
+class SymboleEPRIM : public SymboleNT {
+   public:
+      SymboleEPRIM(SymboleE *e  = nullptr) : SymboleNT(EPRIM, 0), e(e) {}
+      virtual ~SymboleEPRIM() { }
+
+      virtual void Affiche() override;
+      virtual int eval() const override;
+
+      void setE(SymboleE *expression);
+
+   private:
+      SymboleE *e;
 };
