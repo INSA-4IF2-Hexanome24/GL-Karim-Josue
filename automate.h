fbd4c22cba5ff758ee8b48950ece626d456496be
Kakim <163103520+kakiim@users.noreply.github.com>
2026-02-25 09:51:00 +0100

    Add Automate class for parsing symbols and states

---
+++ automate.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <iostream>
+#include "lexer.h"
+#include "symbole.h"
+#include "etats/etat.h"
+#include <stack>
+
+using namespace std;
+
+class Automate {
+    public:
+    
+    Automate(Lexer * l);
+    
+    void Decaler(Symbole *s, Etat *e) {
+        // Empiler le symbole et l'état correspondant
+        pileSymbole.push(s);
+        pileEtats.push(e);
+        lexer->Avancer(); // Avancer le lexer pour lire le prochain symbole
+    }
+
+    void Reduire(int n, Symbole *s) {
+        // Dépiler les n derniers symboles et états
+        for (int i = 0; i < n; ++i) {
+            pileSymbole.pop();
+            pileEtats.pop();
+        }
+        // Empiler le symbole résultant de la réduction
+        pileSymbole.push(s);
+    }
+
+
+    void Accepter() {
+        // L'automate accepte l'entrée
+        std::cout << "Accepté!" << std::endl;
+    }
+    
+    
+    private:
+    Lexer * lexer;
+    stack<Symbole *> pileSymbole;
+    stack<Etat *> pileEtats;
+
+};
