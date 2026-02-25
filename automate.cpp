d469deb929216786a1daf0121eaa2c3d64930513
Kakim <163103520+kakiim@users.noreply.github.com>
2026-02-25 10:40:15 +0100

    Add Analyse method to Automate class
    Implement Analyse method for state machine processing

--- automate.cpp
+++ automate.cpp
@@ -4,3 +4,17 @@
 Automate::Automate(Lexer * l) : lexer(l) {
     pileEtats.push(new Etat0());
 }
+
+void Automate::Analyse() {
+    Symbole * s = lexer->Consulter();
+    while (!accepted) {
+        if (pileEtats.empty()) {
+            std::cerr << "Erreur: pile d'etats vide" << std::endl;
+            break;
+        }
+        Etat * e = pileEtats.top();
+        if (!e) break;
+        e->Transition(s, this);
+        s = lexer->Consulter();
+    }
+}
